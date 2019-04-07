1db2f40d96f78caec307d5aa391863795fbed65a
Swapnil Daingade <swapnil.daingade@gmail.com>
2019-04-07 04:14:09 -0700

    EOD commit 20190407

--- l07/04_applying_sobel.py
+++ l07/04_applying_sobel.py
@@ -17,31 +17,39 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     # Apply the following steps to img
     # 1) Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    #plt.imshow(gray)
     # 2) Take the derivative in x or y given orient = 'x' or 'y'
     if orient == 'x':
         sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
     else:
-        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    #plt.imshow(sobel)
     # 3) Take the absolute value of the derivative or gradient
     abs_sobel = np.absolute(sobel)
     #print("abs_sobel shape: {}".format(abs_sobel.shape))
+    #plt.imshow(abs_sobel)
     # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
     scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    #plt.imshow(scaled_sobel)
     # 5) Create a mask of 1's where the scaled gradient magnitude 
             # is > thresh_min and < thresh_max
     sxbinary = np.zeros_like(scaled_sobel)
     sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+    #plt.imshow(sxbinary)
     # 6) Return this mask as your binary_output image
     binary_output = np.copy(sxbinary) # Remove this line
     return binary_output
     
 # Run the function
-grad_binary = abs_sobel_thresh(image, orient='x', thresh_min=20, thresh_max=100)
+grad_binary_x = abs_sobel_thresh(image, orient='x', thresh_min=20, thresh_max=100)
+grad_binary_y = abs_sobel_thresh(image, orient='y', thresh_min=20, thresh_max=100)
 # Plot the result
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(50, 20))
 f.tight_layout()
 ax1.imshow(image)
 ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(grad_binary, cmap='gray')
-ax2.set_title('Thresholded Gradient', fontsize=50)
+ax2.imshow(grad_binary_x, cmap='gray')
+ax2.set_title('Thresholded Gradient x', fontsize=50)
+ax3.imshow(grad_binary_y, cmap='gray')
+ax3.set_title('Thresholded Gradient y', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
--- l07/05_magnitude_of_gradient.py
+++ l07/05_magnitude_of_gradient.py
@@ -16,19 +16,25 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
     # Apply the following steps to img
     # 1) Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    #plt.imshow(gray)
     # 2) Take the gradient in x and y separately
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    #plt.imshow(sobelx)
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    #plt.imshow(sobely)
     #print("sobelx shape: {}".format(sobelx.shape))
-    
-    # 3) Calculate the magnitude 
+
+    # 3) Calculate the magnitude
     magnitude = np.sqrt(sobelx **2 + sobely ** 2)
+    #plt.imshow(magnitude)
     #print("magnitude shape: {}".format(magnitude.shape))
     # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
     scaled_magnitude = np.uint8(255*magnitude/np.max(magnitude))
+    #plt.imshow(magnitude)
     # 5) Create a binary mask where mag thresholds are met
     sxbinary = np.zeros_like(scaled_magnitude)
     sxbinary[(scaled_magnitude >= mag_thresh[0]) & (scaled_magnitude <= mag_thresh[1])] = 1
+    #plt.imshow(sxbinary)
     # 6) Return this mask as your binary_output image
     binary_output = np.copy(sxbinary) # Remove this line
     return binary_output
